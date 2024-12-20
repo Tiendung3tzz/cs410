1f550664d37d23071c3e2faecfebb28df7c5eae8
Dung3t <tiendung3t@gmail.com>
2024-12-21 00:42:24 +0700

    update app

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if st.button("Phân tích"):
         st.write("Kết quả phân tích:")
         st.write(updated_query)
         st.write(trans_results)
-        main_clip(query, embeddings, model_clip, image_path,image_files)
+        main_clip(trans_results, embeddings, model_clip, image_path,image_files)
         
 
     else:
