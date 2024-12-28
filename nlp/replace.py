2903dd8e379cc752637ed6b982d79be75b80c0f9
Dung3t <tiendung3t@gmail.com>
2024-12-28 22:59:07 +0700

    update replace function

--- nlp/replace.py
+++ nlp/replace.py
@@ -1,13 +1,29 @@
-
 def replace_named_entities(query, results, men_list, woman_list):
     # Sắp xếp kết quả theo vị trí bắt đầu để tránh xáo trộn chỉ số khi thay thế
     results = sorted(results, key=lambda x: x["start"], reverse=True)
+    processed_words = set()  # Lưu trữ các từ đã thay thế
+
     for result in results:
         word = result["word"]
         start = result["start"]
         end = result["end"]
+
         if word in men_list:
-            query = query[:start] + "người đàn ông" + query[end:]
+            if word in processed_words:
+                replacement = word  # Giữ nguyên nếu đã xử lý trước đó
+            else:
+                replacement = "người đàn ông khác"
+                processed_words.add(word)
+
+            query = query[:start] + replacement + query[end:]
+
         elif word in woman_list:
-            query = query[:start] + "người phụ nữ" + query[end:]
+            if word in processed_words:
+                replacement = word  # Giữ nguyên nếu đã xử lý trước đó
+            else:
+                replacement = "người phụ nữ khác"
+                processed_words.add(word)
+
+            query = query[:start] + replacement + query[end:]
+
     return query
