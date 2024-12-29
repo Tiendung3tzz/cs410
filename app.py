db9b00b0b7fbd3c1ba5d0757074c3fdb37f72bc2
Dung3t <tiendung3t@gmail.com>
2024-12-29 19:43:09 +0700

    update visualize

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ if st.button("Phân tích"):
         distances, retrieved_image_files = main_clip(trans_results, embeddings, model_clip, image_path,image_files)
         visualize_results(trans_results, distances, retrieved_image_files)
         max_index, normalized_array = arcface_run(retrieved_image_files,mtcnn,model_arcface,distances,ent_results,threshod)
+        st.write(normalized_array)
         visualize_results(trans_results, normalized_array[max_index], retrieved_image_files,1)
 
     else:
